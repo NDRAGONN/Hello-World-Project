475bd8585b9e1695d4a50c42395a3740814e7a81
NDRAGONN <77333440+NDRAGONN@users.noreply.github.com>
2021-01-22 01:25:32 +0200

    Update new 1e.cs

--- new 1e.cs
+++ new 1e.cs
@@ -12,23 +12,23 @@ void oldest (){
 }
 
 int main() {
-    //int Nouf, Mano, Stefanos;
+    
     int Nouf = 23; int Mano = 21; int Stefanos = 22;
 
     printf (" Emmanouil Katramados, Stefanos David Georgiou, Nouf Alrumaih \n");
 
     if (Mano < Stefanos && Mano < Nouf){
         youngest();
-        //printf (" Emmanouil Katramados, Stefanos David Georgiou, Nouf Alrumaih \n");
+        
     }
 
     else if (Stefanos > Mano && Stefanos < Nouf){
         middle();
-        //printf("Stefanos David Georgiou, Emmanouil Katramados, Nouf Alrumaih ");
+        
         }
     else {
         oldest();
-        //printf("Nouf Alrumaih, Stefanos David Georgiou, Emmanouil Katramados");
+        
     }
 
 
